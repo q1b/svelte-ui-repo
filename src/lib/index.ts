45fcfbd4b07f0094e4b4d9431285dab4b581f3d1
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-04-03 09:48:05 +0530

    added default exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
 // Reexport your entry components here
+
+import * as Button from "./components/ui/button/index.js"
+import * as Card from "./components/ui/card/index.js"
+
+export {
+  Button,
+  Card
+}
